dcfccce51c20c44a6ef84303bb34167f8c9310d8
Trevor Fayas <tfayas@hbs.net>
2021-03-24 15:32:23 -0500

    Updates to project

--- Source-Admin/URLRedirectAdminInitializationModule.cs
+++ Source-Admin/URLRedirectAdminInitializationModule.cs
@@ -1,5 +1,8 @@
-﻿using CMS.DataEngine;
+﻿using CMS;
+using CMS.DataEngine;
 using CMS.Modules;
+
+[assembly: RegisterModule(typeof(URLRedirection.Admin.URLRedirectAdminInitializationModule))]
 
 namespace URLRedirection.Admin
 {
@@ -22,11 +25,11 @@ namespace URLRedirection.Admin
             if (e.ResourceName.Equals("URLRedirection", System.StringComparison.InvariantCultureIgnoreCase))
             {
                 e.Manifest.Metadata.Id = "HBS.KenticoURLRedirection.Admin";
-                e.Manifest.Metadata.ProjectUrl = "https://github.com/silvertech/KenticoURLRedirectionModule";
-                e.Manifest.Metadata.IconUrl = "https://github.com/silvertech/KenticoURLRedirectionModule/blob/master/silvertech-logo.png?raw=true";
+                e.Manifest.Metadata.ProjectUrl = "https://github.com/KenticoDevTrev/KenticoURLRedirectionModule";
+                e.Manifest.Metadata.IconUrl = "https://raw.githubusercontent.com/Kentico/devnet.kentico.com/master/marketplace/assets/generic-integration.png";
                 e.Manifest.Metadata.Copyright = "";
                 e.Manifest.Metadata.Title = "Kentico URL Redirection";
-                e.Manifest.Metadata.ReleaseNotes = "Fixed Staging Issue";
+                e.Manifest.Metadata.ReleaseNotes = "Fixed issue with spaced found in URL redirect causing exceptions.";
                 e.Manifest.Metadata.Tags = "URL Redirection, URL, Redirect, Kentico, MVC";
             }
         }
--- Source/URLRedirection/RedirectionTableEventHandler.cs
+++ Source/URLRedirection/RedirectionTableEventHandler.cs
@@ -20,6 +20,25 @@ namespace URLRedirection
         protected override void OnInit()
         {
             base.OnInit();
+
+            RedirectionTableInfo.TYPEINFO.Events.Insert.Before += InsertUpdate_Before;
+            RedirectionTableInfo.TYPEINFO.Events.Update.Before += InsertUpdate_Before;
         }
+
+        private void InsertUpdate_Before(object sender, ObjectEventArgs e)
+        {
+            RedirectionTableInfo TableObj = (RedirectionTableInfo)e.Object;
+            TableObj.RedirectionOriginalURL = TableObj.RedirectionOriginalURL.Trim();
+            TableObj.RedirectionTargetURL = TableObj.RedirectionTargetURL.Trim();
+
+            // Set Site ID
+            if (TableObj.RedirectionSiteID == 0)
+            {
+                TableObj.RedirectionSiteID = SiteContext.CurrentSiteID;
+                TableObj.Update();
+            }
+        }
+
+       
     }
 }
