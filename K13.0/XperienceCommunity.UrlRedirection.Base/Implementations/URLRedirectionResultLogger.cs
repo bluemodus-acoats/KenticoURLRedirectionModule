36ebd58db42cf755f9295be731cabdac9ac75787
Trevor Fayas <tfayas@hbs.net>
2022-05-19 12:02:41 -0500

    Fix Trailing Slash issue with matches and add Url Logging interfaces

---
+++ K13.0/XperienceCommunity.UrlRedirection.Base/Implementations/URLRedirectionResultLogger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XperienceCommunity.UrlRedirection
+{
+    public class URLRedirectionResultLogger : IURLRedirectionResultLogger
+    {
+        public Task LogSuccessfulMatchAsync(string absoluteUrl, string urlToRedirect, List<string> possibleMatchKeys, string currentCulture, List<RedirectionEntry> foundEntries)
+        {
+            // Do nothing, should implement your own logger
+            return Task.FromResult((object)null);
+        }
+
+        public Task LogUnsuccessfulCultureMatchAsync(string absoluteUrl, List<string> possibleMatchKeys, List<RedirectionEntry> foundEntries, string currentCulture)
+        {
+            // Do nothing, should implement your own logger
+            return Task.FromResult((object)null);
+        }
+
+        public Task LogUnsuccessfulMatchAsync(string absoluteUrl, List<string> possibleMatchKeys, Dictionary<string, List<RedirectionEntry>> pathToRedirectionEntry)
+        {
+            // Do nothing, should implement your own logger
+            return Task.FromResult((object)null);
+        }
+    }
+}
---
+++ K13.0/XperienceCommunity.UrlRedirection.Base/Interfaces/IURLRedirectionResultLogger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XperienceCommunity.UrlRedirection
+{
+    public interface IURLRedirectionResultLogger
+    {
+        Task LogUnsuccessfulMatchAsync(string absoluteUrl, List<string> possibleMatchKeys, Dictionary<string, List<RedirectionEntry>> pathToRedirectionEntry);
+        Task LogUnsuccessfulCultureMatchAsync(string absoluteUrl, List<string> possibleMatchKeys, List<RedirectionEntry> foundEntries, string currentCulture);
+        Task LogSuccessfulMatchAsync(string absoluteUrl, string urlToRedirect, List<string> possibleMatchKeys, string currentCulture, List<RedirectionEntry> foundEntries);
+    }
+}
